7f36d2d3b4e41bd4cfdc99c1a0c3bc259144bde3
iryna-zatynina <ira.zat1997@gmail.com>
2023-09-29 22:56:24 +0200

    add recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipeListComponent,
+    RecipesComponent,
+    RecipeItemComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test', 'test desc', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg')
+  ];
+}
